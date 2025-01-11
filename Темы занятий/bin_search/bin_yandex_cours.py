95c1ea6381d4417145f0d62294df8ce8a43e82ec
Vladislav <146530026+VladPath@users.noreply.github.com>
2025-01-11 10:39:29 +0300

    Реализовал базовое решение бинарного поиска

---
+++ Темы занятий/bin_search/bin_yandex_cours.py
@@ -0,0 +1,23 @@
+# Задание 1: найти число х входящее от 1 до 100
+x = 99
+
+
+# def ans(x,answer):
+#     if x >= answer:
+#         return True
+#     return False
+
+def bin_search(x:int):
+    l = 1
+    r = 100
+    while l<r:
+        m = (l+r) // 2
+        
+        if x <= m:
+            r = m
+        else:
+            l = m+1
+    return l
+
+print(bin_search(x))
+    
